789e19d761299ff485cca89e5801418e5be20730
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-07 21:06:43 +0300

    commit tree 5124e889bac4079dc4c9d86093ce02e284a97df3

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let divide = function(){return 2000 / 100};
-let square = x => x**x;
-let add = (a,b) => a + b;
+const divide = function(){return 2000 / 100};
+const square = x => x**x;
+const add = (a,b) => a + b;
